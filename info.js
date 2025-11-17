38e021bef7d364c1773068ea80172b88a96bb485
Keshav Acharya <keshav.ach@outlook.com>
2025-11-17 14:56:57 -0800

    basic portfolio

---
+++ info.js
@@ -0,0 +1,17 @@
+const PROJECTS = [
+  {
+    name: "Stills (2024)",
+    link: "https://devpost.com/software/stills",
+    description: "A wellness-focused cross-platform mobile application focused on inspiring users with daily photographic prompts."
+  },
+  {
+    name: "UMix (2023)",
+    link: "https://devpost.com/software/umix",
+    description: "Built a browser extension that lets users use AI to live-edit HTML live in the browser."
+  },
+  {
+    name: "Defused (2022)",
+    link: "https://keshavach.wixsite.com/portfolio/defused",
+    description: "A 2D platformer built in Godot about a bomb that doesn't want to explode."
+  }
+];
